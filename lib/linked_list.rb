64c74ef34329f718e3f5476cdfcfc4c671585531
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-07 18:49:11 -0500

    Add pop and includes? methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -27,7 +27,7 @@ class LinkedList
       count = 0
     else
       count = 1
-      current_node = @head
+      current_node = self.head
       while(!current_node.next_node.nil?)
         current_node = current_node.next_node
         count += 1
@@ -38,7 +38,7 @@ class LinkedList
   
   def to_string
     data_string = '' 
-    current_node = @head
+    current_node = self.head
     if(self.head.nil?)
       data_string 
     else
@@ -57,7 +57,7 @@ class LinkedList
       @head = Node.new(data)
     else
       current_head = Node.new(data)
-      current_head.next_node = @head
+      current_head.next_node = self.head
       @head = current_head
     end
     return data
@@ -68,7 +68,7 @@ class LinkedList
       position = @position
       return 'empty'
     else
-      current_node = @head
+      current_node = self.head
       (position - 1).times do
       current_node = current_node.next_node
       end
@@ -95,18 +95,42 @@ class LinkedList
   #   end
   #   return 
   # end
-  def find(position, num_elements)
-    current_node = self.head
-    (position).times do    
-      until(current_node == nil || current_node.next_node == nil || current_node.position == position) do
-      current_node = current_node.next_node
-      num_elements = position.to_string
 
+  # def find(position, num_elements)
+  #   current_node = self.head
+  #   (position).times do    
+  #     until(current_node == nil || current_node.next_node == nil || current_node.position == position) do
+  #     current_node = current_node.next_node
+  #     num_elements = position.to_string
+
+  #   end
+  #   end
+  #   return 'empty' if current_node == nil
+  #   return current_node.num_elements
+  # end
+
+  def includes?(data)
+    current_node = self.head   
+      until(current_node == nil || current_node.next_node == nil || current_node.data == data) do
+      current_node = current_node.next_node
     end
-    end
-    return false if current_node == nil
-    return current_node.num_elements
+    return false if current_node.data != data
+    return true
   end
+
+  def pop
+    if(self.head.nil?)
+      return 'empty'
+    else current_node = self.head
+      while (!current_node.next_node.next_node.nil?)
+      current_node = current_node.next_node
+      end
+    tail_node = current_node.next_node
+    current_node.next_node = nil
+  end
+    tail_node
+  end
+
 
 
 end
